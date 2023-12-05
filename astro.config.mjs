5ea6f985ff781bb54f5f8225204c56fcd5618bbd
ExcaliBear <amitrock6789@gmail.com>
2023-12-05 07:27:56 +0200

    Add support for Latex in Markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,28 @@ export default defineConfig({
   integrations: [tailwind()],
   output: "hybrid",
   adapter: vercel(),
+  markdown: {
+    remarkPlugins: ["remark-math"],
+    rehypePlugins: [
+      [
+        "rehype-katex",
+        {
+          macros: {
+            "\\E": "\\mathbb{E}",
+            "\\C": "\\mathbb{C}",
+            "\\R": "\\mathbb{R}",
+            "\\N": "\\mathbb{N}",
+            "\\Q": "\\mathbb{Q}",
+            "\\bigO": "\\mathcal{O}",
+            "\\abs": "|#1|",
+            "\\set": "\\{ #1 \\}",
+            "\\indep": "{\\perp\\mkern-9.5mu\\perp}",
+            "\\nindep": "{\\not\\!\\perp\\!\\!\\!\\perp}",
+            "\\latex": "\\LaTeX",
+            "\\katex": "\\KaTeX",
+          },
+        },
+      ],
+    ],
+  },
 })
